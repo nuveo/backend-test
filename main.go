d376d928018b32d6dbc76d1cd9580628265b5b22
Lauren Ferreira <lauren.@gmail.com>
2018-09-16 19:23:13 -0300

    Removing creation queries, renaming non-idiomatic variable names

--- main.go
+++ main.go
@@ -10,20 +10,6 @@ var (
 	queue ItemQueue
 )
 
-const createEnum = `DO $$ BEGIN
-CREATE TYPE status_t AS ENUM ('inserted', 'consumed');
-EXCEPTION
-WHEN duplicate_object THEN null;
-END $$;`
-const createExtension = `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`
-const createTable = `CREATE TABLE IF NOT EXISTS workflows (
-	uuid UUID DEFAULT uuid_generate_v4(),
-	status status_t DEFAULT 'inserted' NOT NULL,
-	data JSONB NOT NULL,
-	steps text[] NOT NULL,
-	PRIMARY KEY (uuid)
-);`
-
 // Credential contains user credentials.
 type Credential struct {
 	User     string
@@ -35,21 +21,21 @@ func main() {
 	if len(os.Args) <= 3 {
 		log.Fatalf("Failed to start application: arguments are missing")
 	}
-	c := Credential{User: os.Args[1], Password: os.Args[2], Database: os.Args[3]}
+	credential := Credential{User: os.Args[1], Password: os.Args[2], Database: os.Args[3]}
 
-	var a App
-	if err := a.Connect(c.User, c.Password, c.Database); err != nil {
+	var app App
+	if err := app.Connect(credential.User, credential.Password, credential.Database); err != nil {
 		log.Fatalf("Failed to open a database connection: %v", err)
 	}
 
-	if err := a.Prepare(); err != nil {
+	if err := app.Database(); err != nil {
 		log.Fatalf("Failed to prepare database: %v", err)
 	}
 
-	a.Routes()
+	app.Routes()
 
 	queue.New()
 
 	log.Printf("Application running on localhost%s", port)
-	a.Run(port)
+	app.Run(port)
 }
