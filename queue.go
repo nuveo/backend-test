df10bcfefff66fc3ed9eae3c7f249702b94eb919
Lauren Ferreira <lauren.@gmail.com>
2018-09-16 18:57:42 -0300

    Creating queue and its methods

---
+++ queue.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"sync"
+)
+
+// ItemQueue is a queue of workflow Items.
+type ItemQueue struct {
+	items []Workflow
+	lock  sync.RWMutex
+}
+
+// New creates a new workflow ItemQueue.
+func (w *ItemQueue) New() *ItemQueue {
+	w.items = []Workflow{}
+
+	return w
+}
+
+// Enqueue adds a workflow Item to the end of the queue.
+func (w *ItemQueue) Enqueue(i Workflow) {
+	w.lock.Lock()
+	w.items = append(w.items, i)
+	w.lock.Unlock()
+}
+
+// Dequeue removes a workflow Item from the first position of the queue.
+func (w *ItemQueue) Dequeue() *Workflow {
+	w.lock.Lock()
+	workflow := w.items[0]
+	w.items = w.items[1:len(w.items)]
+	w.lock.Unlock()
+
+	return &workflow
+}
+
+// Front returns the first Item in the queue without removing it.
+func (w *ItemQueue) Front() *Workflow {
+	w.lock.RLock()
+	workflow := w.items[0]
+	w.lock.RUnlock()
+
+	return &workflow
+}
+
+// IsEmpty verifies if the queue is empty.
+func (w *ItemQueue) IsEmpty() bool {
+	return len(w.items) == 0
+}
+
+// Size returns the number of Items in the queue.
+func (w *ItemQueue) Size() int {
+	return len(w.items)
+}
