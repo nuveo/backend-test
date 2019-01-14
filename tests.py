a4cdf678d383fc49f8e2d8600f8e91ec60d20839
ronald <ronald-farias@outlook.com>
2019-01-14 15:13:25 -0300

    improve client declaration and add test_post_200 testcase

--- tests.py
+++ tests.py
@@ -7,20 +7,35 @@ from app import app
 
 
 class TestWorkflowSimple(unittest.TestCase):
+    client = app.test_client()
     def test_post_400(self):
         """
             Test if the body is a json valid
         """
-        client = app.test_client()
-        self.assertEqual(client.post('/workflow/', data='wrong json').status_code, 400)
+        self.assertEqual(self.client.post('/workflow/', data='wrong json').status_code, 400)
 
     def test_post_403(self):
         """
             Test if status passed in body are between inserted or consumed
         """
-        client = app.test_client()
         data = {'status': 'inexistent status'}
-        self.assertEqual(client.post('/workflow/', json=data).status_code, 403)
+        self.assertEqual(self.client.post('/workflow/', json=data).status_code, 403)
+
+    def test_post_200(self):
+        data = {
+            "data": {
+                "field1": "value1",
+                "child1": {
+                    "field1": "value1"
+                    }
+                },
+                "child2": {
+                    "field1": "value1"
+                },
+            "status": "inserted",
+            "steps": ["step1", "step2", "step3"]
+        }
+        self.assertEqual(self.client.post('/workflow/', json=data).status_code, 201)
 
 
 if __name__ == '__main__':
