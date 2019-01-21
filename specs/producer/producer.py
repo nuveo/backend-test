9437641110843f2bc59524bbf63df09e17f89246
ronald <ronald-farias@outlook.com>
2019-01-21 10:23:46 -0300

    add environ vars about connections and get them into const dictionaries

--- specs/producer/producer.py
+++ specs/producer/producer.py
@@ -3,14 +3,27 @@
     to the workflow_queue rabbitmq queue
 """
 import os
+from os.path import join, dirname
 import select
 import pika
 import psycopg2
 import psycopg2.extensions
+from dotenv import load_dotenv
+
+load_dotenv(join(dirname(__file__), '.env'))
+
+RABBITMQ_HOST = os.getenv('RBMQ_HOST')
+CONNECTION_INFO = {
+    'host': os.getenv('PG_HOST'),
+    'database': os.getenv('PG_DATABASE'),
+    'user': os.getenv('PG_USER'),
+    'password': os.getenv('PG_PASSWORD'),
+    'port': os.getenv('PG_PORT')
+}
 
 
 def rabbit_notify(message):
-    connection = pika.BlockingConnection(pika.ConnectionParameters('rbmq'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST))
     channel = connection.channel()
 
     channel.queue_declare(queue='workflow_queue')
@@ -19,13 +32,6 @@ def rabbit_notify(message):
 
     connection.close()
 
-CONNECTION_INFO = {
-    'host': 'db',
-    'database': 'postgres',
-    'user': 'postgres',
-    'password': 'postgres',
-    'port': 5432
-}
 
 conn = psycopg2.connect(**CONNECTION_INFO)
 conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
@@ -43,4 +49,3 @@ while 1:
             notify = conn.notifies.pop(0)
             rabbit_notify(notify.payload)
             print('Obteve NOTIFY:', notify.pid, notify.channel, notify.payload)
-
