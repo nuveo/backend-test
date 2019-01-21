7f021052a0a4ed68cb81371eade052a686c64994
ronald <ronald-farias@outlook.com>
2019-01-21 10:32:56 -0300

    fix pep issues, whitespaces

--- specs/consumer/consumer.py
+++ specs/consumer/consumer.py
@@ -19,8 +19,10 @@ channel = connection.channel()
 
 channel.queue_declare(queue='workflow_queue')
 
+
 def callback(ch, method, properties, body):
     print(' [x] Received %r' % body)
+
 
 channel.basic_consume(callback, queue='workflow_queue', no_ack=True)
 print(' [*] Waiting for messages. To exit press CTRL+C')
