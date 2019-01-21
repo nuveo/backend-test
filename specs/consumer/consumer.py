a078e2e2102bd371b07e75ef4a930d7ab802e222
ronald <ronald-farias@outlook.com>
2019-01-21 08:54:33 -0300

    move producer and consumer source files to your own modules

--- specs/consumer/consumer.py
+++ specs/consumer/consumer.py
@@ -1,14 +1,14 @@
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('rbmq'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='workflow_queue')
 
 def callback(ch, method, properties, body):
     print(' [x] Received %r' % body)
 
-channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.basic_consume(callback, queue='workflow_queue', no_ack=True)
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
 channel.start_consuming()
--- specs/producer/producer.py
+++ specs/producer/producer.py
@@ -8,20 +8,23 @@ import pika
 import psycopg2
 import psycopg2.extensions
 
+
 def rabbit_notify(message):
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rbmq'))
     channel = connection.channel()
 
     channel.queue_declare(queue='workflow_queue')
-    channel.basic_publish(exchange='', routing_key='workflow', body=message)
+    channel.basic_publish(exchange='', routing_key='workflow_queue', body=message)
     print(' [x] Sent ', message)
 
     connection.close()
 
 CONNECTION_INFO = {
-    'database': os.getenv('API_PREST_DB_NAME'),
-    'user': '',
-    'password': ''
+    'host': 'db',
+    'database': 'postgres',
+    'user': 'postgres',
+    'password': 'postgres',
+    'port': 5432
 }
 
 conn = psycopg2.connect(**CONNECTION_INFO)
@@ -30,7 +33,7 @@ conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 curs = conn.cursor()
 curs.execute('LISTEN workflow_insert;')
 
-print('Esperando notificações no canal \'test\'')
+print('Esperando notificações no canal \'workflow_insert\'')
 while 1:
     if select.select([conn], [], [], 5) == ([], [], []):
         print('Timeout')
