5280b51081e550bf0e613dd3922070c183464592
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2020-12-05 16:17:07 -0300

    Adds new routers

---
+++ router/router.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"backend-test/middlewares"
+
+	"github.com/gorilla/mux"
+)
+
+// Router is exported and used in main.go
+func Router() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/workflow", middlewares.GetAllWorkflow).Methods("GET", "OPTIONS")
+	router.HandleFunc("/workflow", middlewares.CreateWorkflow).Methods("POST", "OPTIONS")
+	router.HandleFunc("/workflow/{uuid}", middlewares.UpdateWorkflow).Methods("PATCH", "OPTIONS")
+	//router.HandleFunc("/workflow/consume", middlewares.ConsumeWorkflow).Methods("GET", "OPTIONS")
+
+	return router
+}
