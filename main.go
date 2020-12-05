cb38ee4c3b136bd80067bc7c195f9cf7b22ee0ba
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2020-12-05 15:25:13 -0300

    Adds folder structure

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from Docker")
+	})
+
+	fmt.Println("Listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
